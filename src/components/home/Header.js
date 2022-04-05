0dfcb1382d4e966ffde63e6bd9fc56ea30c54156
naerim <naerim1119@gmail.com>
2022-04-06 03:13:55 +0900

    [FEAT] 헤더 반응형 UI

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import Logo from '../../assets/logo_header.png';
+import { Menu } from '@mui/icons-material';
+import { tablet } from '../../utils/responsive';
 
 const Header = () => {
+  const [toggle, setToggle] = useState(true);
   return (
     <Container>
-      <Image src={Logo} />
-      <Button>편지쓰기</Button>
+      <Wrap>
+        <Image src={Logo} />
+        <Center toggle={toggle}>
+          <MenuItem>소개</MenuItem>
+          <MenuItem>받은편지</MenuItem>
+          <MenuItem>보낸편지</MenuItem>
+          <MenuItem>환자관리</MenuItem>
+          <MenuItem>공지사항</MenuItem>
+        </Center>
+      </Wrap>
+      <Right toggle={toggle}>
+        <AuthText>로그인</AuthText>
+        <Button>편지쓰기</Button>
+      </Right>
+      <Hamburger onClick={() => setToggle(!toggle)}>
+        <Menu sx={{ fontSize: 30 }} />
+      </Hamburger>
     </Container>
   );
 };
@@ -15,11 +34,52 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 20px;
+  ${tablet({ flexDirection: 'column' })}
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  ${tablet({ flexDirection: 'column' })}
 `;
 
 const Image = styled.img`
   height: 30px;
   width: 70px;
+`;
+
+const Center = styled.div`
+  display: flex;
+  margin-left: 120px;
+  ${tablet({ marginLeft: '0px', textAlign: 'center', marginTop: '20px' })}
+  @media (max-width: 768px) {
+    display: ${(props) => (props.toggle ? 'block' : 'none')};
+  }
+`;
+
+const MenuItem = styled.div`
+  font-size: 16px;
+  color: #3e3e3e;
+  cursor: pointer;
+  margin-right: 20px;
+  ${tablet({ padding: '16px', marginRight: '0px' })}
+`;
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  ${tablet({ flexDirection: 'column' })}
+  @media (max-width: 768px) {
+    display: ${(props) => (props.toggle ? 'block' : 'none')};
+  }
+`;
+
+const AuthText = styled.text`
+  font-size: 16px;
+  color: #3e3e3e;
+  cursor: pointer;
+  margin-right: 20px;
+  ${tablet({ padding: '16px', textAlign: 'center', marginRight: '0px' })}
 `;
 
 const Button = styled.button`
@@ -33,5 +93,14 @@ const Button = styled.button`
   &:hover {
     transform: scale(1.1);
   }
+  ${tablet({ alignItem: 'center', marginTop: '10px' })}
 `;
+
+const Hamburger = styled.div`
+  display: none;
+  position: absolute;
+  right: 20px;
+  ${tablet({ display: 'block' })}
+`;
+
 export default Header;
