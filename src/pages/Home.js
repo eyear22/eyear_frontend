4c792c018fe229a8ca1badd6a42db84be7bfd1bc
naerim <naerim1119@gmail.com>
2022-04-09 23:48:08 +0900

    [FEAT] 메인페이지 item layout 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,19 @@ import styled from 'styled-components';
 import Banner from '../components/home/Banner';
 import Footer from '../components/home/Footer';
 import Header from '../components/home/Header';
+import Layout from '../components/home/Layout';
 
 const Home = () => {
   return (
     <Container>
       <Header />
       <Banner />
-      <div style={{ backgroundColor: '#f5f5f5', height: '100vh' }}>Home</div>
+      <Layout title="아이어 소개">
+        <div>Home</div>
+      </Layout>
+      <Layout title="문의하기">
+        <div>Home</div>
+      </Layout>
       <Footer />
     </Container>
   );
