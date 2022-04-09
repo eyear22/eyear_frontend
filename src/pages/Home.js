4cae680e972d39eda8f0503ad4153bff3f680f34
naerim <naerim1119@gmail.com>
2022-04-09 17:14:41 +0900

    [FEAT] Footer 생성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
+import styled from 'styled-components';
+import Footer from '../components/home/Footer';
 import Header from '../components/home/Header';
 
 const Home = () => {
   return (
-    <div>
+    <Container>
       <Header />
-      <div style={{ backgroundColor: '#f5f5f5', height: 500 }}>Home</div>
-    </div>
+      <div style={{ backgroundColor: '#f5f5f5', height: '100vh' }}>Home</div>
+      <Footer />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  background-color: #f5f5f5;
+`;
 
 export default Home;
