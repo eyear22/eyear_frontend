e25bca32cb81c5bf7b0097e9364b6430424f9b38
naerim <naerim1119@gmail.com>
2022-04-14 20:03:06 +0900

    [FEAT] 받는 사람 first input 받아오기

--- src/components/send/ReceiverInput.js
+++ src/components/send/ReceiverInput.js
@@ -1,15 +1,68 @@
 import { Autocomplete, CircularProgress, TextField } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+const sleep = (delay = 0) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
+const topFilms = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+];
+
 const ReceiverInput = () => {
+  const [open, setOpen] = useState(false);
   const [options, setOptions] = useState([]);
-  const loading = false;
+  const loading = open && options.lengt === 0;
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+    (async () => {
+      await sleep(1e3);
+
+      if (active) {
+        setOptions([...topFilms]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
   return (
     <Container>
       <Title>받는 사람</Title>
       <Input
-        options={options}
+        open={open}
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        isOptionEqualToValue={(option, value) => option.title === value.title}
+        getOptionLabel={(option) => option.title}
+        options={topFilms}
+        loading={loading}
         sx={{ maxWidth: 200 }}
         renderInput={(params) => (
           <TextField
