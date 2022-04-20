cf188e163728c43b0833cbab4048113ddd3ae42e
naerim <naerim1119@gmail.com>
2022-04-20 22:19:26 +0900

    [FEAT] 로그인 페이지 생성

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import AuthLayout from '../components/common/AuthLayout';
+
+const Login = () => {
+  return <AuthLayout title="로그인">로그인</AuthLayout>;
+};
+
+const Container = styled.div`
+  background-color: #fff;
+`;
+
+export default Login;
