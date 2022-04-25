a949bfd9e2bf7acc58448b80c22314c83fe29ad8
naerim <naerim1119@gmail.com>
2022-04-25 10:13:47 +0900

    [CHORE] tabMenu 레이아웃으로 정리

---
+++ src/components/common/TabMenuLayout.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import { mobile } from '../../utils/responsive';
+
+const TabMenuLayout = ({ idx, Component1, Component2 }) => {
+  const tabClickHandler = (v) => idx.onChange(v);
+  const tabMenu = [
+    {
+      title: (
+        <TitleContainer key={0} active={!idx.value} onClick={() => tabClickHandler(0)}>
+          <Title active={!idx.value}>개인</Title>
+        </TitleContainer>
+      ),
+      content: Component1,
+    },
+    {
+      title: (
+        <TitleContainer key={1} active={idx.value} onClick={() => tabClickHandler(1)}>
+          <Title active={idx.value}>기관</Title>
+        </TitleContainer>
+      ),
+      content: Component2,
+    },
+  ];
+
+  return (
+    <Container>
+      <Wrap>
+        <TopBar>
+          {tabMenu.map((section, index) => {
+            return section.title;
+          })}
+        </TopBar>
+        <TopLine />
+        <div>{tabMenu[idx.value].content}</div>
+      </Wrap>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 40px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Wrap = styled.div`
+  border: 1px solid #889287;
+  width: 400px;
+  ${mobile({ width: '300px' })}
+`;
+
+const TopBar = styled.div`
+  display: flex;
+  cursor: pointer;
+`;
+
+const TitleContainer = styled.div`
+  width: 200px;
+  text-align: center;
+  padding: 12px 0;
+  ${mobile({ width: '150px' })}
+  background-color: ${(props) => (props.active ? '#626a61' : '#fff')};
+`;
+
+const Title = styled.text`
+  color: ${(props) => (props.active ? '#fff' : '#889287')};
+`;
+
+const TopLine = styled.div`
+  height: 2px;
+  background-color: #626a61;
+`;
+
+export default TabMenuLayout;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,82 +1,18 @@
-import { useState } from 'react';
-import styled from 'styled-components';
 import AuthLayout from '../components/common/AuthLayout';
+import TabMenuLayout from '../components/common/TabMenuLayout';
 import Input from '../components/login/Input';
-import { mobile } from '../utils/responsive';
+import useInput from '../utils/useInput';
 
 const Login = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const tabClickHandler = (idx) => setActiveIndex(idx);
-  const tabMenu = [
-    {
-      title: (
-        <TitleContainer key={0} active={!activeIndex} onClick={() => tabClickHandler(0)}>
-          <Title active={!activeIndex}>개인</Title>
-        </TitleContainer>
-      ),
-      content: <Input />,
-    },
-    {
-      title: (
-        <TitleContainer key={1} active={activeIndex} onClick={() => tabClickHandler(1)}>
-          <Title active={activeIndex}>기관</Title>
-        </TitleContainer>
-      ),
-      content: <Input />,
-    },
-  ];
+  const activeIndex = useInput(0);
+  const id = useInput('');
+  const password = useInput('');
 
   return (
     <AuthLayout title="로그인">
-      <Container>
-        <Wrap>
-          <TopBar>
-            {tabMenu.map((section, index) => {
-              return section.title;
-            })}
-          </TopBar>
-          <TopLine />
-          <div>{tabMenu[activeIndex].content}</div>{' '}
-        </Wrap>
-      </Container>
+      <TabMenuLayout idx={activeIndex} Component1={<Input />} Component2={<Input />} />
     </AuthLayout>
   );
 };
-
-const Container = styled.div`
-  margin-top: 40px;
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Wrap = styled.div`
-  border: 1px solid #889287;
-  width: 400px;
-  ${mobile({ width: '300px' })}
-`;
-
-const TopBar = styled.div`
-  display: flex;
-  cursor: pointer;
-`;
-
-const TitleContainer = styled.div`
-  width: 200px;
-  text-align: center;
-  padding: 12px 0;
-  ${mobile({ width: '150px' })}
-  background-color: ${(props) => (props.active ? '#626a61' : '#fff')};
-`;
-
-const Title = styled.text`
-  color: ${(props) => (props.active ? '#fff' : '#889287')};
-`;
-
-const TopLine = styled.div`
-  height: 2px;
-  background-color: #626a61;
-`;
 
 export default Login;
