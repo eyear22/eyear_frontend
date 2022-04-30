021fc812d924d7edf4915671bd21785744c6b0a0
naerim <naerim1119@gmail.com>
2022-04-30 19:08:56 +0900

    [ADD] icon 추가

--- src/components/join/SelectBox.js
+++ src/components/join/SelectBox.js
@@ -1,25 +1,60 @@
 import styled from 'styled-components';
+import UserIcon from '../../assets/icon_user.png';
 import { mobile } from '../../utils/responsive';
 
 const SelectBox = () => {
-  return <Container>SelectBox</Container>;
+  return (
+    <Container>
+      <Left>
+        <Icon src={UserIcon} alt="" />
+      </Left>
+      <Right>
+        <Desc>영상을 주고 받고 싶다면?</Desc>
+        <Title>개인 회원</Title>
+      </Right>
+    </Container>
+  );
 };
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
   border: 1px solid #889287;
+  border-radius: 10px;
   align-items: center;
   width: 500px;
   height: 160px;
   margin-bottom: 20px;
   cursor: pointer;
-  -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
-  box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
-  ${mobile({ width: 280 })}
+  box-shadow: 2px 2px 4px 2px #dfdfdf;
+  ${mobile({ width: 280, height: 200, flexDirection: 'column', justifyContent: 'center' })}
   &:hover {
     width: 502px;
     height: 162px;
-    ${mobile({ width: 282 })}
+    ${mobile({ width: 282, height: 202 })}
   }
+`;
+
+const Left = styled.div`
+  margin: 0 50px;
+`;
+
+const Icon = styled.img`
+  width: 90px;
+  height: 90px;
+`;
+
+const Right = styled.div`
+  ${mobile({ textAlign: 'center', marginTop: 10 })}
+`;
+
+const Desc = styled.text`
+  color: #737272;
+  font-size: 16px;
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
 `;
 
 export default SelectBox;
