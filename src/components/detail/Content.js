44b4cf38b2f9998ab4c0f90b9a893705083281dd
naerim <naerim1119@gmail.com>
2022-05-08 21:31:10 +0900

    [FEAT] 이미지 슬라이드 완성

--- src/components/detail/Content.js
+++ src/components/detail/Content.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
-import Test from '../../assets/img_home.jpg';
 import ImageSlide from './ImageSlide';
 
 const Content = () => {
   return (
     <Container>
-      <Image src={Test} />
       <ImageSlide />
-      <Wrap>엄마 잘 지내고 있지?</Wrap>
+      <Wrap>
+        {'엄마 잘 지내고 있지?'} <div /> {'나'}
+      </Wrap>
     </Container>
   );
 };
@@ -18,12 +18,8 @@ const Container = styled.div`
   padding: 10px;
 `;
 
-const Image = styled.img`
-  width: 100%;
-`;
-
 const Wrap = styled.div`
-  margin-top: 20px;
+  margin: 20px;
   line-height: 160%;
   text-decoration: underline;
 `;
--- src/components/detail/ImageSlide.js
+++ src/components/detail/ImageSlide.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Test from '../../assets/img_home.jpg';
 
 const ImageSlide = () => {
   const settings = {
@@ -8,26 +11,39 @@ const ImageSlide = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0px',
   };
 
-  const data = [{}];
+  const data = [
+    { key: 0, image: Test },
+    { key: 1, image: Test },
+  ];
   return (
-    <Container>
-      <Slider {...settings}>
-        <div>
-          <h2>1</h2>
-        </div>
-        <div>
-          <h2>2</h2>
-        </div>
-      </Slider>
-    </Container>
+    <StyledSlider {...settings}>
+      {data.map(({ key, image }) => {
+        return (
+          <CardBox key={key}>
+            <CardImg src={image} />
+          </CardBox>
+        );
+      })}
+    </StyledSlider>
   );
 };
 
-const Container = styled.div`
+const StyledSlider = styled(Slider)`
+  .slick-prev::before,
+  .slick-next::before {
+    color: #889287;
+  }
+  margin: 10px 20px;
+`;
+
+const CardBox = styled.div``;
+
+const CardImg = styled.img`
   width: 100%;
-  background-color: aliceblue;
 `;
 
 export default ImageSlide;
