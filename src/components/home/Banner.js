d245befbf6a96775416518931eb1693ca982ce48
naerim <naerim1119@gmail.com>
2022-05-19 01:40:38 +0900

    [FEAT] 메인페이지 배경화면 변경

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,24 +1,27 @@
 import styled from 'styled-components';
-import HomeImage from '../../assets/img_home.jpg';
+import Img from '../../assets/img_background.jpg';
 import { mobile } from '../../utils/responsive';
 
 const Banner = () => {
   return (
     <Container>
-      <Image src={HomeImage} alt="homeImage" />
-      <SubTitle>더 뚜렷한 요양병원 소통 플랫폼</SubTitle>
-      <Title>아이어</Title>
-      <Button>문의하기</Button>
+      <Image src={Img} alt="homeImage" />
+      <TextDiv>
+        <SubTitle>더 뚜렷한 요양병원 소통 플랫폼</SubTitle>
+        <Title>아이어</Title>
+        <Button>문의하기</Button>
+      </TextDiv>
     </Container>
   );
 };
 
 const Container = styled.div`
   width: 100vw;
-  height: 60vh;
+  height: 50vh;
   position: relative;
   margin-top: 70px;
   background-color: black;
+  ${mobile({ height: '40vh' })}
 `;
 
 const Image = styled.img`
@@ -28,29 +31,27 @@ const Image = styled.img`
   opacity: 0.7;
 `;
 
-const SubTitle = styled.h2`
+const TextDiv = styled.div`
   position: absolute;
-  color: white;
-  font-size: 32px;
   top: 20%;
   left: 10vw;
+`;
+
+const SubTitle = styled.h2`
+  color: white;
+  font-size: 32px;
   ${mobile({ fontSize: 26 })}
 `;
 
 const Title = styled.h1`
-  position: absolute;
   color: white;
   font-size: 58px;
-  top: 27%;
-  left: 10vw;
   ${mobile({ fontSize: 40 })}
 `;
 
 const Button = styled.button`
-  position: absolute;
+  margin-top: 10vh;
   color: white;
-  left: 10vw;
-  bottom: 20%;
   padding: 10px;
   background-color: transparent;
   border: 2px solid #fff;
