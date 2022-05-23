c9e3e5959555482900c490a4d0e5a8858d23a0dc
naerim <naerim1119@gmail.com>
2022-05-23 22:36:50 +0900

    [CHORE] 소개 리팩토링

--- src/components/home/Introduce.js
+++ src/components/home/Introduce.js
@@ -6,6 +6,33 @@ import Icon4 from '../../assets/icon_introduce4.png';
 import { mobile } from '../../utils/responsive';
 import Layout from './Layout';
 
+const items = [
+  {
+    key: 0,
+    img: Icon1,
+    title: '아이어',
+    subTitle: '영상을 주고받으며 ' + '\n' + ' 소통할 수 있는 플랫폼 서비스',
+  },
+  {
+    key: 1,
+    img: Icon2,
+    title: '베리어프리 서비스',
+    subTitle: '소통의 어려움을 해결하기 위해 ' + '\n' + '영상 자동 자막 생성 기능 제작',
+  },
+  {
+    key: 2,
+    img: Icon3,
+    title: '사용 방법',
+    subTitle: '편지 쓰기를 통해 영상 전송 ' + '\n' + '보낸 편지를 통해 확인',
+  },
+  {
+    key: 3,
+    img: Icon4,
+    title: '인증된 병원',
+    subTitle: '보건의료빅데이터개방시스템을 통해 ' + '\n' + '요양병원 인증 절차 진행',
+  },
+];
+
 const Introduce = () => {
   return (
     <Layout title="아이어 소개" id="introduce">
@@ -13,41 +40,13 @@ const Introduce = () => {
         아이어는 요양병원을 이용하는 노인과 가족들간의 면회 상황에서 노인들이 겪는 불편함을 발견하면서 시작됐습니다.
       </Desc>
       <Wrap>
-        <Item>
-          <Image src={Icon1} />
-          <Title>아이어</Title>
-          <SubTitle>
-            영상을 주고받으며 <br />
-            소통할 수 있는 플랫폼 서비스
-          </SubTitle>
-        </Item>
-        <Item>
-          <Image src={Icon2} />
-          <Title>베리어프리 서비스</Title>
-          <SubTitle>
-            소통의 어려움을 해결하기 위해
-            <br />
-            영상 자동 자막 생성 기능 제작
-          </SubTitle>
-        </Item>
-        <Item>
-          <Image src={Icon3} />
-          <Title>사용 방법</Title>
-          <SubTitle>
-            편지 쓰기를 통해 영상 전송
-            <br />
-            보낸 편지를 통해 확인
-          </SubTitle>
-        </Item>
-        <Item>
-          <Image src={Icon4} />
-          <Title>인증된 병원</Title>
-          <SubTitle>
-            보건의료빅데이터개방시스템을 통해
-            <br />
-            요양병원 인증 절차 진행
-          </SubTitle>
-        </Item>
+        {items.map(({ key, img, title, subTitle }) => (
+          <Item key={key}>
+            <Image src={img} />
+            <Title>{title}</Title>
+            <SubTitle>{subTitle}</SubTitle>
+          </Item>
+        ))}
       </Wrap>
     </Layout>
   );
@@ -67,6 +66,7 @@ const Wrap = styled.div`
   justify-content: center;
   margin-bottom: 100px;
   ${mobile({ flexDirection: 'column' })}
+  white-space: pre-wrap;
 `;
 
 const Item = styled.div`
