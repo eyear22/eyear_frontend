828d03a8139b882fe95ba2f16e5f77f51b48f685
naerim <naerim1119@gmail.com>
2022-06-03 22:16:41 +0900

    [FEAT] 아이디, 이메일, 비밀번호 확인 함수 생성

--- src/components/joinUser/SecondInput.js
+++ src/components/joinUser/SecondInput.js
@@ -1,27 +1,87 @@
 import styled from 'styled-components';
+import { publicRequest } from '../../hooks/requestMethods';
 import { mobile } from '../../utils/responsive';
 import useInput from '../../utils/useInput';
 
 const SecondInput = ({ activeIndex, email, userId, sex, username, password, passwordCheck, join }) => {
-  const err = useInput('');
+  // 아이디/이메일 체크여부
+  const checkId = useInput(false);
+  const checkEmail = useInput(false);
+
+  const errPwd = useInput('');
+  const errPwdCheck = useInput('');
   // 이전 버튼 눌렀을 때
   const onPrev = () => activeIndex.onChange(0);
-
   // 성별 선택
   const changeSex = (v) => sex.onChange(v);
+
+  // 비밀번호 형식 확인
+  const checkPwdRegex = (pwd) => {
+    if (pwd == ' ' || pwd == '') {
+      errPwd.onChange('비밀번호를 입력해주세요.');
+    } else if (
+      !/[0-9]/.test(pwd) ||
+      !/[a-zA-Z]/.test(pwd) ||
+      !/[~!@#$%<>^&*]/.test(pwd) ||
+      pwd.length < 8 ||
+      pwd.length > 16
+    ) {
+      errPwd.onChange('8~16문자 영문, 숫자, 특수문자를 사용하세요.');
+    } else {
+      errPwd.onChange('');
+    }
+  };
 
   // 비밀번호 확인
   const checkPwdInput = (pwd, checkPwd) => {
     if (pwd == checkPwd) {
-      err.onChange('');
+      errPwdCheck.onChange('');
     } else {
-      err.onChange('비밀번호가 일치하지 않습니다.');
+      errPwdCheck.onChange('비밀번호가 일치하지 않습니다.');
+    }
+  };
+
+  // 이메일 확인
+  const confirmEmail = async () => {
+    const regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    if (email.value == '') {
+      alert('이메일을 입력해주세요.');
+    } else if (!regex.test(email.value)) {
+      alert('이메일 형식이 올바르지 않습니다.');
+    } else {
+      try {
+        // 이메일 중복 확인
+        const res = await publicRequest.get(`/join/user_email_check/${email.value}`);
+        if (res.data == 'exit') alert('중복되는 이메일이 있습니다.');
+        else {
+          alert('사용가능한 이메일입니다.');
+          checkEmail.onChange(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  // 아이디 확인
+  const confirmId = async () => {
+    try {
+      // 아이디 중복 확인
+      const res = await publicRequest.get(`/join/user_id_check/${userId.value}`);
+      if (res.data == 'exit') alert('중복되는 아이디가 있습니다.');
+      else {
+        alert('사용가능한 아이디입니다.');
+        checkId.onChange(true);
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
   // null값이 있는지 확인
   const checkNull =
-    email.value == '' ||
+    !checkId.value ||
+    !checkEmail.value ||
     userId.value == '' ||
     username.value == '' ||
     password.value == '' ||
@@ -34,10 +94,13 @@ const SecondInput = ({ activeIndex, email, userId, sex, username, password, pass
         <Right>
           <Input
             value={email.value}
-            onChange={(e) => email.onChange(e.target.value)}
+            onChange={(e) => {
+              email.onChange(e.target.value);
+              checkEmail.onChange(false);
+            }}
             placeholder="이메일을 입력해주세요."
           />
-          <RightButton>중복확인</RightButton>
+          <RightButton onClick={confirmEmail}>중복확인</RightButton>
         </Right>
       </Wrap>
       <Wrap>
@@ -45,10 +108,13 @@ const SecondInput = ({ activeIndex, email, userId, sex, username, password, pass
         <Right>
           <Input
             value={userId.value}
-            onChange={(e) => userId.onChange(e.target.value)}
+            onChange={(e) => {
+              userId.onChange(e.target.value);
+              checkId.onChange(false);
+            }}
             placeholder="아이디를 입력해주세요."
           />
-          <RightButton>중복확인</RightButton>
+          <RightButton onClick={confirmId}>중복확인</RightButton>
         </Right>
       </Wrap>
       <Wrap>
@@ -70,14 +136,18 @@ const SecondInput = ({ activeIndex, email, userId, sex, username, password, pass
           placeholder="이름을 입력해주세요."
         />
       </Wrap>
-      <Wrap>
+      <Wrap style={{ margin: 0 }}>
         <Title>비밀번호</Title>
         <FullInput
           value={password.value}
-          onChange={(e) => password.onChange(e.target.value)}
+          onChange={(e) => {
+            password.onChange(e.target.value);
+            checkPwdRegex(e.target.value);
+          }}
           placeholder="비밀번호를 입력해주세요."
         />
       </Wrap>
+      <Error style={{ marginBottom: 10 }}>{errPwd.value}</Error>
       <Wrap style={{ margin: 0 }}>
         <Title>비밀번호 확인</Title>
         <FullInput
@@ -89,7 +159,7 @@ const SecondInput = ({ activeIndex, email, userId, sex, username, password, pass
           placeholder="비밀번호를 입력해주세요."
         />
       </Wrap>
-      <Error>{err.value}</Error>
+      <Error>{errPwdCheck.value}</Error>
       <Bottom>
         <NextButton onClick={() => onPrev()}>이전</NextButton>
         <NextButton disabled={checkNull} onClick={() => join()}>
@@ -202,6 +272,7 @@ const Error = styled.div`
   margin-left: 135px;
   color: red;
   font-size: 10px;
+  ${mobile({ marginLeft: 0 })}
 `;
 
 export default SecondInput;
