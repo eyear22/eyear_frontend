e2c1afb7cd7e7e74c993613276e2661450556383
naerim <naerim1119@gmail.com>
2022-07-02 21:47:01 +0900

    [FEAT] 기업 아이디 중복 확인

--- src/components/joinBusiness/SecondInfo.js
+++ src/components/joinBusiness/SecondInfo.js
@@ -1,60 +1,63 @@
 import styled from 'styled-components';
+import { publicRequest } from '../../hooks/requestMethods';
 import { mobile } from '../../utils/responsive';
+import ButtonLayout from './ButtonLayout';
+import InputLayout from './InputLayout';
 
 const SecondInfo = ({ activeIndex, email, userId, password, passwordCheck, join }) => {
   // 이전 버튼 눌렀을 때
   const onPrev = () => activeIndex.onChange(0);
+
+  const checkId = async () => {
+    if (checkId.value == '') {
+      alert('아이디를 입력해주세요.');
+    } else {
+      try {
+        // 아이디 중복 확인
+        const res = await publicRequest.get(`/join/business_id_check/${userId.value}`);
+        if (res.data == 'exit') {
+          alert('중복되는 아이디가 있습니다.');
+          userId.onChange('');
+        } else {
+          alert('사용가능한 아이디입니다.');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <Container>
-      <Wrap>
-        <Title>이메일</Title>
-        <Right>
-          <Input
-            value={email.value}
-            onChange={(e) => {
-              email.onChange(e.target.value);
-            }}
-            placeholder="이메일을 입력해주세요."
-          />
-          <RightButton>중복확인</RightButton>
-        </Right>
-      </Wrap>
-      <Wrap>
-        <Title>아이디</Title>
-        <Right>
-          <Input
-            value={userId.value}
-            onChange={(e) => {
-              userId.onChange(e.target.value);
-            }}
-            placeholder="아이디를 입력해주세요."
-          />
-          <RightButton>중복확인</RightButton>
-        </Right>
-      </Wrap>
-      <Wrap style={{ margin: 0 }}>
-        <Title>비밀번호</Title>
-        <FullInput
-          type="password"
-          value={password.value}
-          onChange={(e) => {
-            password.onChange(e.target.value);
-          }}
-          placeholder="비밀번호를 입력해주세요."
-        />
-      </Wrap>
-      {/* <Error style={{ marginBottom: 10 }}>{errPwd.value}</Error> */}
-      <Wrap style={{ margin: 0 }}>
-        <Title>비밀번호 확인</Title>
-        <FullInput
-          type="password"
-          value={passwordCheck.value}
-          onChange={(e) => {
-            passwordCheck.onChange(e.target.value);
-          }}
-          placeholder="비밀번호를 입력해주세요."
-        />
-      </Wrap>
+      <ButtonLayout
+        title="이메일"
+        placeholder="이메일을 입력해주세요."
+        btnText="중복확인"
+        value={email.value}
+        onChange={(e) => email.onChange(e.target.value)}
+        btnClick={() => console.log('d')}
+      />
+      <ButtonLayout
+        title="아이디"
+        placeholder="아이디를 입력해주세요."
+        btnText="중복확인"
+        value={userId.value}
+        onChange={(e) => userId.onChange(e.target.value)}
+        btnClick={checkId}
+      />
+      <InputLayout
+        title="비밀번호"
+        value={password.value}
+        onChange={(e) => password.onChange(e.target.value)}
+        placeholder="비밀번호를 입력해주세요."
+      />
+      {/* <Error>s</Error> */}
+      <InputLayout
+        title="비밀번호 확인"
+        value={passwordCheck.value}
+        onChange={(e) => passwordCheck.onChange(e.target.value)}
+        placeholder="비밀번호를 입력해주세요."
+      />
       {/* <Error>{errPwdCheck.value}</Error> */}
       <Bottom>
         <NextButton onClick={() => onPrev()}>이전</NextButton>
@@ -144,6 +147,8 @@ const NextButton = styled.button`
 `;
 
 const Error = styled.div`
+  position: relative;
+  bottom: 20px;
   margin-left: 135px;
   color: red;
   font-size: 10px;
