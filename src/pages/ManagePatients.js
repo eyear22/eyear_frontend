a6624ce7257d01a8b3040d7d82e54b5200fb6062
naerim <naerim1119@gmail.com>
2022-07-08 23:22:47 +0900

    [FEAT] 환자 관리 테이블

--- src/pages/ManagePatients.js
+++ src/pages/ManagePatients.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
+import TopImg from '../assets/img_receive.png';
+import ImgLayout from '../components/common/ImgLayout';
 
 const ManagePatients = () => {
-  return <div>ManagePatients</div>;
+  return (
+    <ImgLayout title="환자 관리" src={TopImg} width={900}>
+      <table>
+        <thead>
+          <td>환자</td>
+          <td>환자</td>
+        </thead>
+      </table>
+    </ImgLayout>
+  );
 };
 
 export default ManagePatients;
