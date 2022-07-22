e816c32afdfaa4fe42de909423da37fb65a93ace
naerim <naerim1119@gmail.com>
2022-07-22 23:52:09 +0900

    [FEAT] 공지사항 디테일 페이지 글 부분

--- src/pages/NoticeDetail.js
+++ src/pages/NoticeDetail.js
@@ -5,22 +5,37 @@ const NoticeDetail = () => {
     <>
       <Wrap>
         <Title>서비스 일시 중지안내(07/21)</Title>
-        <Line />
+        <HR />
+        <Desc>서비스 일시 중지안내 관련 공지사항 입니다.</Desc>
       </Wrap>
+      <Button>목록</Button>
     </>
   );
 };
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  min-height: 400px;
+`;
 
 const Title = styled.h1`
   font-size: 26px;
 `;
 
-const Line = styled.div`
-  width: 100px;
-  height: 10px;
-  color: #222;
+const HR = styled.hr`
+  margin: 20px 0;
+`;
+
+const Desc = styled.div``;
+
+const Button = styled.button`
+  background-color: #889287;
+  padding: 10px;
+  width: 160px;
+  color: #fff;
+  border: none;
+  border-radius: 6px;
+  box-shadow: 2px 2px 4px 2px #dfdfdf;
+  cursor: pointer;
 `;
 
 export default NoticeDetail;
