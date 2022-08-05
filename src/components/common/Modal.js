3021ce4891785e6d977b33a7d60bceae6216a2e1
naerim <naerim1119@gmail.com>
2022-08-05 20:52:16 +0900

    [FEAT] 모달창 생성

---
+++ src/components/common/Modal.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+const Modal = () => {
+  return (
+    <Container>
+      <Wrap>
+        <CloseButton id="modalCloseBtn">X</CloseButton>
+      </Wrap>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+
+const Wrap = styled.div`
+  position: absolute;
+  width: 300px;
+  height: 500px;
+  padding: 40px;
+  text-align: center;
+  background-color: rgb(255, 255, 255);
+  border-radius: 10px;
+  box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  border: none;
+  color: rgba(0, 0, 0, 0.7);
+  background-color: transparent;
+  font-size: 20px;
+`;
+
+export default Modal;
--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -5,6 +5,8 @@ import TopArea from '../components/notice/TopArea';
 import FilterSelect from '../components/notice/FilterSelect';
 import useInput from '../utils/useInput';
 import NoticeTable from '../components/notice/NoticeTable';
+import NoticePostButton from '../components/notice/NoticePostButton';
+import Modal from '../components/common/Modal';
 
 const dummy = [
   {
@@ -32,10 +34,23 @@ const Notice = () => {
         <FilterSelect filter={filter} input={input} />
         <NoticeTable list={dummy} isNotNull={true} />
       </Wrap>
+      <NoticePostButton />
+      <Modal />
     </ImgLayout>
   );
 };
 
 const Wrap = styled.div``;
+
+const Button = styled.button`
+  background-color: #889287;
+  padding: 10px;
+  width: 160px;
+  color: #fff;
+  border: none;
+  border-radius: 6px;
+  box-shadow: 2px 2px 4px 2px #dfdfdf;
+  cursor: pointer;
+`;
 
 export default Notice;
