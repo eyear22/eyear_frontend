176b7f2e39531e8c192183fb2a0452e6ff419b17
naerim <naerim1119@gmail.com>
2022-08-20 19:58:19 +0900

    [FEAT] 공지사항 등록 api 연결

--- src/components/notice/AddNoticeModal.js
+++ src/components/notice/AddNoticeModal.js
@@ -1,24 +1,52 @@
+import axios from 'axios';
 import styled from 'styled-components';
+import { publicRequest } from '../../hooks/requestMethods';
+import useInput from '../../utils/useInput';
 import Modal from '../common/Modal';
 
 const AddNoticeModal = ({ open }) => {
+  const title = useInput('');
+  const content = useInput('');
+
+  const addNotice = () => {
+    axios({
+      method: 'post',
+      url: `http://localhost:5000/notice`,
+      data: { title: title.value, content: content.value },
+    })
+      .then((res) => {
+        console.log(res.data);
+        alert('공지사항이 등록되었습니다.');
+        open.onChange(false);
+      })
+      .catch(() => alert('공지사항 등록에 실패하였습니다.'));
+  };
+
   return (
     <Modal open={open}>
       <Container>
         <Wrap>
           <Title>제목</Title>
           <InputArea>
-            <Input />
+            <Input
+              value={title.value}
+              onChange={(e) => title.onChange(e.target.value)}
+              placeholder="제목을 입력해주세요."
+            />
           </InputArea>
         </Wrap>
         <Wrap>
           <Title>내용</Title>
           <InputArea>
-            <LongInput />
+            <LongInput
+              value={content.value}
+              onChange={(e) => content.onChange(e.target.value)}
+              placeholder="내용을 입력해주세요."
+            />
           </InputArea>
         </Wrap>
         <ButtonArea>
-          <Button>작성하기</Button>
+          <Button onClick={addNotice}>작성하기</Button>
         </ButtonArea>
       </Container>
     </Modal>
