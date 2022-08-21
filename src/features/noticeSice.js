0f0b48105cc7320c4b5a8db3dcef5ec7362495c6
naerim <naerim1119@gmail.com>
2022-08-21 22:22:10 +0900

    [FEAT] 공지사항 reducer

---
+++ src/features/noticeSice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const noticeSlice = createSlice({
+  name: 'notice',
+  initialState: {
+    notices: null, // 보낸편지 리스트
+    noticesFetching: false,
+    noticesError: false,
+  },
+  reducers: {
+    fetchNoticeStart: (state) => {
+      state.noticesFetching = true;
+    },
+    fetchNoticeSuccess: (state, action) => {
+      state.noticesFetching = false;
+      state.notices = action.payload;
+    },
+    fetchNoticeFailure: (state) => {
+      state.noticesFetching = false;
+      state.noticesError = true;
+    },
+  },
+});
+
+export const { fetchNoticeStart, fetchNoticeSuccess, fetchNoticeFailure } = noticeSlice.actions;
+export default noticeSlice.reducer;
