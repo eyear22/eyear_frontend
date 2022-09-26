9fa7ba2481ba00ea8d80b2e7e0a93ab201e456c0
naerim <naerim1119@gmail.com>
2022-09-26 20:08:31 +0900

    [FEAT] 비밀번호 변경 버튼 추가

--- src/components/changePwd/ChangePwdSuccessContainer.js
+++ src/components/changePwd/ChangePwdSuccessContainer.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import Layout from '../common/Layout';
 import DoneIcon from '../../assets/icon_joinDone.png';
+import { useNavigate } from 'react-router-dom';
 
 const ChangePwdSuccessContainer = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Layout title="" width={600} bgColor="#fff">
@@ -10,6 +12,14 @@ const ChangePwdSuccessContainer = () => {
           <Icon src={DoneIcon} />
           <Title>비밀번호 변경 완료</Title>
         </Wrap>
+        <ButtonWrap>
+          <button className="leftBtn" onClick={() => navigate('/')}>
+            홈으로
+          </button>
+          <button className="rightBtn" onClick={() => navigate('/login')}>
+            로그인
+          </button>
+        </ButtonWrap>
       </Layout>
     </>
   );
@@ -31,6 +41,29 @@ const Icon = styled.img`
 const Title = styled.div`
   font-size: 34px;
   font-weight: bold;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  margin-top: 40px;
+  justify-content: center;
+  .leftBtn {
+    width: 140px;
+    padding: 16px;
+    color: #889287;
+    background: #fff;
+    border: 1px solid #889287;
+    cursor: pointer;
+    margin-right: 20px;
+  }
+  .rightBtn {
+    width: 140px;
+    padding: 16px;
+    color: #ffffff;
+    background: #626a61;
+    border: 1px solid #626a61;
+    cursor: pointer;
+  }
 `;
 
 export default ChangePwdSuccessContainer;
